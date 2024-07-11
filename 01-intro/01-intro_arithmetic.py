50a4339751cbde6b7c55cfcba090b3b855708f7f
njlyon0 <nicholasjlyon@gmail.com>
2024-07-11 11:37:34 -0400

    Adding first bit of day 1 code to a dedicated sub-folder

---
+++ 01-intro/01-intro_arithmetic.py
@@ -0,0 +1,11 @@
+# Start with simple arithmetic
+print(4 + 3)
+print(1 - 2)
+print(2 * 8)
+print(7 / 6)
+## Exponent operator is '**'
+print(3 ** 2)
+## Remainder found by '%'
+print(1 % 2)
+## Remainder removed by '//'
+print(1 // 2)
