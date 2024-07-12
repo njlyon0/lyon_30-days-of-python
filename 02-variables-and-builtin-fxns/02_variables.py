fec877b4cb971a51ac2f9f61215f3d5713d46a85
njlyon0 <nicholasjlyon@gmail.com>
2024-07-12 13:37:41 -0400

    Prepping 02 exercises and finishing notes script

--- 02-variables-and-builtin-fxns/02_variables.py
+++ 02-variables-and-builtin-fxns/02_variables.py
@@ -3,6 +3,17 @@ Variables are how Python stores data in computer memory
 These variables can be invoked / operated on as aliases for the data they contain
 
 Note that variable names are case-sensitive and can only contain letters/numbers or underscores
+Snake case (e.g., "my_variable") is a common convention for Python programmers
 """
 
+# Variables can be assigned in a process called "variable declaration" with an equal sign
+test_var = [1, 2, 3]
+print(test_var)
 
+# You can declare multiple variables in the same line if desired
+test_var1, test_var2, test_var3 = "hello", "world", "!"
+print([test_var1, test_var2, test_var3])
+
+# Variables can be defined interactively in the Terminal with the 'input' function
+# first_name = input("Type your name:")
+# print(first_name)
