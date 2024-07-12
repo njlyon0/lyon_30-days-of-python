ea397c302bda42f270452b1138d1e1cd645a6a7a
njlyon0 <nicholasjlyon@gmail.com>
2024-07-12 13:25:34 -0400

    Beginning variables script

---
+++ 02-variables-and-builtin-fxns/02_variables.py
@@ -0,0 +1,8 @@
+"""
+Variables are how Python stores data in computer memory
+These variables can be invoked / operated on as aliases for the data they contain
+
+Note that variable names are case-sensitive and can only contain letters/numbers or underscores
+"""
+
+
