c2fe1021c714a557105fb5070fe0ca98a2dc7cc1
njlyon0 <nicholasjlyon@gmail.com>
2024-07-12 13:51:53 -0400

    Finished day 2 exercises! Commented out interactive bits

--- 02-variables-and-builtin-fxns/02_exercises.py
+++ 02-variables-and-builtin-fxns/02_exercises.py
@@ -1,63 +1,108 @@
 # Exercises: Level 1
 
 ## Inside 30DaysOfPython create a folder called day_2. Inside this folder create a file named variables.py
+### Just going to use this script so I can use a different naming convention
 
 ## Write a python comment saying 'Day 2: 30 Days of python programming'
+### 'Day 2: 30 Days of python programming'
 
 ## Declare a first name variable and assign a value to it
+first_name = "Nick"
 
 ## Declare a last name variable and assign a value to it
+last_name = "Lyon"
 
 ## Declare a full name variable and assign a value to it
+full_name = "Nick Lyon"
 
 ## Declare a country variable and assign a value to it
+country = "USA"
 
 ## Declare a city variable and assign a value to it
+city = "SB"
 
 ## Declare an age variable and assign a value to it
+age = 73.3
 
 ## Declare a year variable and assign a value to it
+year = 2024
 
 ## Declare a variable is_married and assign a value to it
+is_married = False
 
 ## Declare a variable is_true and assign a value to it
+is_true = True
 
 ## Declare a variable is_light_on and assign a value to it
+light_on = False
 
 ## Declare multiple variables on one line
+var_first, var_second = "hello", "world"
+
 
 # Exercises: Level 2
 
 ## Check the data type of all your variables using type() built-in function
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(light_on))
+print(type(var_first))
+print(type(var_second))
 
 ## Using the len() built-in function, find the length of your first name
+print(len(first_name))
 
 ## Compare the length of your first name and your last name
+print(len(first_name) - len(last_name))
 
 ## Declare 5 as num_one and 4 as num_two
+num_one, num_two = 5, 4
 
 ## Add num_one and num_two and assign the value to a variable total
+variable_total = num_one + num_two
 
 ## Subtract num_two from num_one and assign the value to a variable diff
+variable_diff = num_two - num_one
 
 ## Multiply num_two and num_one and assign the value to a variable product
+variable_product = num_one * num_two
 
 ## Divide num_one by num_two and assign the value to a variable division
+variable_div = num_one / num_two
 
 ## Use modulus division to find num_two divided by num_one and assign the value to a variable remainder
+variable_remain = num_one % num_two
 
 ## Calculate num_one to the power of num_two and assign the value to a variable exp
+variable_exp = num_one ** num_two
 
 ## Find floor division of num_one by num_two and assign the value to a variable floor_division
+variable_floor_div = num_one // num_two
 
 ## The radius of a circle is 30 meters.
+radius = 30
 
 ## Calculate the area of a circle and assign the value to a variable name of area_of_circle
+area_of_circle = 3.14 * (radius ** 2)
 
 ## Calculate the circumference of a circle and assign the value to a variable name of circum_of_circle
+circum_of_circle = 2 * 3.14 * radius
 
 ## Take radius as user input and calculate the area.
+# user_radius = float(input("Enter the radius of a hypothetical circle:"))
+# user_area = 3.14 * (user_radius ** 2)
+# print("The area of that circle is", user_area)
 
 ## Use the built-in input function to get first name, last name, country and age from a user and store the value to their corresponding variable names
+# user_first, user_last, user_country, user_age = input("Type your first name:"), input("Type your last name:"), input("Type your country:"), input("Type your age:")
+# print(user_first, user_last, "is", user_age, "years old and lives in", user_country)
 
 ## Run help('keywords') in Python shell or in your file to check for the Python reserved words or keywords
+# print(help("keywords"))
