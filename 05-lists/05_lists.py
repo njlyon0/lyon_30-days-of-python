5b498c21a2fbdfdf0a51239528803dbc351f4084
njlyon0 <nicholasjlyon@gmail.com>
2024-07-18 12:16:37 -0400

    Adding more day 5 notes

--- 05-lists/05_lists.py
+++ 05-lists/05_lists.py
@@ -32,8 +32,67 @@ print("The second fruit in our list is: ", fruits[1])
 ## Note that the first 'reverse index' is "-1" *not* 0 as it is in regular indexing
 print("The penultimate fruit in our list is: ", fruits[-2])
 
+# We can also "unpack" list items similarly to how we'd assign several list items on the same row
+first_fruit, *other_fruits = fruits
+print(first_fruit, type(first_fruit))
+print(other_fruits, type(other_fruits))
 
+## Another example
+num_list = [1, 2, 3, 4, 5, 6]
+first_num, *mid_nums, last_num = num_list
+print(first_num)
+print(mid_nums)
+print(last_num)
 
+"""Slicing"""
+# Lists can be sliced like strings
+## A particular index position
+print(fruits[2])
+
+## From one position to another
+print(fruits[1:3])
+
+## From one position onwards
+print(fruits[1:])
+
+## Every nth item
+print(fruits[::2])
+
+"""Modifying"""
+# Can tweak existing lists via index positions
+## Replace the second fruit
+fruits[1] = "tomato"
+print(fruits)
+
+# Can also check what's in a list
+print("tomato" in fruits)
+
+# Items can be added with the 'append' method (available to list type data) 
+## Note that this method lets us skip assigning a new variable (automatically edits the existing list variable)
+fruits.append("lime")
+print("append 'lime': ", fruits)
+
+# Items can also be inserted between existing items with the 'insert' method
+fruits.insert(2, "apple")
+print("insert 'apple': ", fruits)
+
+# Or items can be removed with the eponymous method
+fruits.remove("tomato")
+print("remove 'tomato': ", fruits)
+
+# We can also use the 'pop' method to remove an item by its index (rather than naming it specifically with 'remove')
+fruits.pop(-2)
+print("pop second-to-last index position: ", fruits)
+
+# We can delete lists entirely with the 'del' keyword
+## Note that keywords are not the same as functions/methods/etc.
+ex_list = [1, 2, 3]
+del ex_list
+
+# Or we can keep the list variable declared but remove everything from it with the 'clear' method
+ex_list = [1, 2, 3]
+ex_list.clear()
+print("Cleared list: ", ex_list)
 
 
 
