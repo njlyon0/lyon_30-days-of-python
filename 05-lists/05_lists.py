1747c08ad4a5cf00beb43fc6282a967c682adaad
njlyon0 <nicholasjlyon@gmail.com>
2024-07-18 15:26:55 -0400

    Finished day 5 notes

--- 05-lists/05_lists.py
+++ 05-lists/05_lists.py
@@ -94,16 +94,60 @@ ex_list = [1, 2, 3]
 ex_list.clear()
 print("Cleared list: ", ex_list)
 
+"""
+**Copying**
 
+You may have noticed the assigning an existing list to a new variable does not work as you may expect
+Doing so and editing the second variable *still affects the first variable*
 
+To truly create a copy of a variable we need to use the 'copy' method
+"""
 
+# Demo copying
+ex_list = [1, 2, 3]
+ex_list2 = ex_list.copy()
+ex_list2.pop(0)
+print(ex_list2)
 
+"""Joining/Concatenating"""
 
+# You can concatenate lists with the '+' operator
+ex_listA = [1, 2, 3]
+ex_listB = [4, 5, 6]
+ex_listAB = ex_listA + ex_listB
+print(ex_listAB)
 
+# You can also do this the the 'extend' method on the first list
+## Note this does change the first list rather than making a new combo list
+ex_listAA = ex_listA.copy()
+ex_listAA.extend(ex_listB)
+print(ex_listAA)
 
+"""Counting & Finding"""
 
+# We can count the number of times an item appears in a list with the 'count' method
+fruits = ["orange", "orange", "orange", "banana"]
+print(fruits.count("orange"))
 
+# Or the 'index' method finds the first item in a list that matches the provided value
+print(fruits.index("banana"))
 
+"""Ordering"""
 
+# Reverse list order with the 'reverse' method
+ex_list3 = [1, 2, 3]
+ex_list3.reverse()
+print(ex_list3)
 
+# Sort with the 'sort' method
+rand_list = [5, 1, 6, 3, 4, 2]
+rand_list.sort()
+print(rand_list)
 
+# Specifying descending with the 'reverse' argument
+rand_list.sort(reverse = True)
+print(rand_list)
+
+# The 'sorted' function shows the sorted list without actually changing the order
+rand_list = [5, 1, 6, 3, 4, 2]
+print(sorted(rand_list))
