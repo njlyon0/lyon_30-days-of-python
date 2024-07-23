d795719de29985f9b7677c0b8e3b7842990e0e5b
njlyon0 <nicholasjlyon@gmail.com>
2024-07-23 13:46:20 -0400

    Day 6 exercises (level 1 only)

---
+++ 06-tuples/06_exercises.py
@@ -0,0 +1,33 @@
+# Exercises: Level 1
+
+## 1. Create an empty tuple
+empty_tup = tuple()
+
+## 2. Create a tuple containing names of your sisters and your brothers (imaginary siblings are fine)
+brother_tup = ("bro 1", "bro 2", "bro 3")
+sister_tup = ("sis 1", "sis 2", "sis 3")
+
+## 3. Join brothers and sisters tuples and assign it to siblings
+siblings_tup = brother_tup + sister_tup
+
+## 4. How many siblings do you have?
+print("4: I have {} siblings".format(len(siblings_tup)))
+
+## 5. Modify the siblings tuple and add the name of your father and mother and assign it to family_members
+family_tup = ("parent 1", "parent 2") + siblings_tup
+print("5: My family consists of: ", family_tup)
+
+# Exercises: Level 2
+
+# Unpack siblings and parents from family_members
+# Create fruits, vegetables and animal products tuples. Join the three tuples and assign it to a variable called food_stuff_tp.
+# Change the about food_stuff_tp tuple to a food_stuff_lt list
+# Slice out the middle item or items from the food_stuff_tp tuple or food_stuff_lt list.
+# Slice out the first three items and the last three items from food_staff_lt list
+# Delete the food_staff_tp tuple completely
+# Check if an item exists in tuple:
+# Check if 'Estonia' is a nordic country
+
+# Check if 'Iceland' is a nordic country
+
+# nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
