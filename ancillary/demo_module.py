95f6891f6e0ce0c99cd8101a4c46a91c4f6ebe9f
njlyon0 <nicholasjlyon@gmail.com>
2024-11-12 11:59:02 -0500

    feat: adding more content to demo module

--- ancillary/demo_module.py
+++ ancillary/demo_module.py
@@ -1,3 +1,18 @@
 # Demo function for the module
 def generate_full_name(firstname, lastname):
     return firstname + ' ' + lastname
+
+# Another demo function
+def sum_two_nums (num1, num2):
+    return num1 + num2
+
+# Define a variable in the module
+gravity = 9.81
+
+# Define a more complex variable
+person = {
+    "firstname": "Asabeneh",
+    "age": 250,
+    "country": "Finland",
+    "city": "Helsinki"
+}
