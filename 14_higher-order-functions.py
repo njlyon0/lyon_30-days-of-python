d61b2ca3277d14dd666b6a33fc7f13c848c4e02c
njlyon0 <nicholasjlyon@gmail.com>
2024-11-15 12:13:51 -0500

    feat (day 14): higher order fxns notes and exercises

--- 14_higher-order-functions.py
+++ 14_higher-order-functions.py
@@ -1,5 +1,124 @@
 """
+Higher Order Functions
 
+Functions can accept one or more functions as parameters and/or they can return a function.
+
+They can also be edited and assigned to variables
 """
 
+""" Functions as Parameters """
+# Define a higher order function
+def ex_hof(_f, _list):
+    result = _f(_list)
+    return result
 
+# Invoke it
+ex_hof(sum, [1, 2, 3, 4])
+
+""" Returning Functions from Functions """
+# Define some helper functions
+def square(x):
+    return x ** 2
+
+def cube(x):
+    return x ** 3
+
+def absolute(x):
+    if x < 0:
+        val = x * -1
+    else: 
+        val = x
+    return val
+
+# Define another HOF
+def ex_hof2(operation):
+    if operation == "square":
+        return square
+    elif operation == "cube":
+        return cube
+    elif operation == "absolute":
+        return absolute
+
+# Invoke it a few ways
+new_fxn = ex_hof2("square")
+print(new_fxn(-2))
+new_fxn = ex_hof2("cube")
+print(new_fxn(-2))
+new_fxn = ex_hof2("absolute")
+print(new_fxn(-2))
+
+""" Python Closures & Decorators """
+
+# Closures are created by defining a function inside of another and returning the nested one
+def add_ten():
+    def add(num):
+        return num + 10
+    return add
+
+# Invoke higher order function
+closure_fxn = add_ten()
+
+# Use the closure function
+print(closure_fxn(5))
+
+# Decorators allow users to _add_ functionality to an existing variable without modifying its structure
+def greeting():
+    return "Welcome!"
+
+def uppercase_decorator(fxn):
+    def wrapper():
+        func = fxn()
+        to_upper = func.upper()
+        return to_upper
+    return wrapper
+
+# Add decorator
+upper_greeting = uppercase_decorator(greeting)
+
+# Demonstrate its changed behavior
+print(greeting())
+print(upper_greeting())
+
+# Create another decorator
+def split_string_decorator(fxn):
+    def wrapper():
+        func = fxn()
+        split_string = func.split()
+        return split_string
+    return wrapper
+
+# Split greeting
+split_greeting = split_string_decorator(greeting)
+
+# Add both decorators
+split_upper_greeting = split_string_decorator(uppercase_decorator(greeting))
+
+# Invoke it
+print(greeting())
+print(split_greeting())
+print(split_upper_greeting())
+
+""" Built-In HOFs """
+
+# The "map" function is a built-in HOF that accepts a function and an iterable as parameters
+def square(x):
+    return x ** 2
+
+nums = [1, 2, 3, 4, 5, 6]
+result = list(map(square, nums))
+print(result)
+
+# Can also do this with lambda functions to write fewer lines
+result = list(map(lambda n: n ** 2, nums))
+print(result)
+
+# The "filter" function returns only items in the iterable that return `True` for the provided conditional
+result = list(filter(lambda i: i % 2 == 0, nums))
+print(result)
+
+# The "reduce" function from the "functools" module is also an HOF
+## It returns a single value rather than another iterable (as with "map" and "reduce")
+import functools
+
+result = functools.reduce(lambda j, k: int(j) + int(k), ["1", "2", "3", "4"])
+print(result)
--- _exercises/14_exercises.py
+++ _exercises/14_exercises.py
@@ -1,3 +1,97 @@
 # Exercises - Higher Order Functions
 
+## Assign neded demo variables
+countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+names = ['Asabeneh', 'Lidiya', 'Ermias', 'Abraham']
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+## Exercises: Level 1
+
+# 1. Explain the difference between map, filter, and reduce.
+"""
+All three accept a function and an interable. Map returns an iterable of the same length as the input, filter returns only those items that evalue to `True`, and reduce returns a single value
+"""
+
+# 2. Explain the difference between higher order function, closure and decorator
+"""
+Higher order functions (HOFs) are those that can accept functions as inputs or return a function as an output. Closures and decorators are types of HOFs. Closures are nested functions where the 'inner' function can access the variables declared in the 'outer' function. Decorators add functionality to existing functions but don't modify the fundamental structure of those functions
+"""
+
+# 3. Define a call function before map, filter or reduce, see examples.
+"""This is not sufficiently explained for 'call function' to mean something specific to me"""
+
+# 4. Use for loop to print each country in the countries list.
+for nation in countries:
+    print(nation)
+
+# 5. Use for to print each name in the names list.
+for nm in names:
+    print(nm)
+
+# 6. Use for to print each number in the numbers list.
+for val in numbers:
+    print(val)
+
+## Exercises: Level 2
+
+# 1. Use map to create a new list by changing each country to uppercase in the countries list
+result = list(map(lambda k: k.upper(), countries))
+print("2-1: ", result)
+
+# 2. Use map to create a new list by changing each number to its square in the numbers list
+result = list(map(lambda n: n ** 2, numbers))
+print("2-2: ", result)
+
+# 3. Use map to change each name to uppercase in the names list
+result = list(map(lambda k: k.upper(), names))
+print("2-3: ", result)
+
+# 4. Use filter to filter out countries containing 'land'.
+result = list(filter(lambda j: not "land" in j, countries))
+print("2-4: ", result)
+
+# 5. Use filter to filter out countries having exactly six characters.
+result = list(filter(lambda l: len(l) != 6, countries))
+print("2-5: ", result)
+
+# 6. Use filter to filter out countries containing six letters and more in the country list.
+result = list(filter(lambda l: len(l) < 6, countries))
+print("2-6: ", result)
+
+# 7. Use filter to filter out countries starting with an 'E'
+result = list(filter(lambda o: not o[0] == "E", countries))
+print("2-7: ", result)
+
+# 8. Chain two or more list iterators (eg. arr.map(callback).filter(callback).reduce(callback))
+
+
+# 9. Declare a function called get_string_lists which takes a list as a parameter and then returns a list containing only string items.
+
+
+# 10. Use reduce to sum all the numbers in the numbers list.
+import functools
+result = functools.reduce(lambda x, y: int(x) + int(y), numbers)
+print("2-10: ", result)
+
+# 11. Use reduce to concatenate all the countries and to produce this sentence: Estonia, Finland, Sweden, Denmark, Norway, and Iceland are north European countries
+
+
+# 12. Declare a function called categorize_countries that returns a list of countries with some common pattern (you can find the [countries list](https://github.com/Asabeneh/30-Days-Of-Python/blob/master/data/countries.py) in this repository as countries.js. (eg 'land', 'ia', 'island', 'stan')).
+
+
+# 13. Create a function returning a dictionary, where keys stand for starting letters of countries and values are the number of country names starting with that letter.
+
+
+# 14. Declare a get_first_ten_countries function - it returns a list of first ten countries from the countries.js list in the data folder.
+
+
+# 15. Declare a get_last_ten_countries function that returns the last ten countries in the countries list.
+
+# ### Exercises: Level 3
+
+# 1. Use the countries_data.py (https://github.com/Asabeneh/30-Days-Of-Python/blob/master/data/countries-data.py) file and follow the tasks below:
+#    - Sort countries by name, by capital, by population
+#    - Sort out the ten most spoken languages by location.
+#    - Sort out the ten most populated countries.
+
 
