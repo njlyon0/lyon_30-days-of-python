1073ae333ad7a2d5ce8258ffd329bd77a14cca6e
njlyon0 <nicholasjlyon@gmail.com>
2024-11-20 14:45:11 -0500

    feat (day 16): notes/exercises on datetime module

--- 16_datetime-module.py
+++ 16_datetime-module.py
@@ -1,11 +1,58 @@
 """
-Python `datatime` Module
+Python `datetime` Module
 
-
+Python has a 'datetime' module for handling dates/times
 """
 
+# Import module
+import datetime
 
+# Check out available content
+print(dir(datetime))
 
+# Get date/time information
+now = datetime.datetime.now()
+now
 
+# Access different parts of "now" via attributes
+now.day
+now.year
+now.second
 
+# Can also use certain methods
+## "Timestamp" is the number of seconds since Jan. 1, 1970
+now.timestamp()
 
+# Can also use the datatime function to format integers as date/times
+new_year = datetime.datetime(2025, 1, 1)
+type(new_year)
+
+# Can format strings with shorthand for time units with the "strftime" method
+## More on format abbreviations here: https://strftime.org/
+now_time = now.strftime("%H:%M:%S")
+now_time
+
+# Can format human-readable string dates with the "strptime" method
+date_str = "1 January, 2025"
+date_obj = datetime.datetime.strptime(date_str, "%d %B, %Y")
+date_obj
+
+# For simpler operations, can use the "date" function
+my_date = datetime.date(2025, 1, 1)
+my_date
+
+# Or can use time objects for time w/o date
+my_time = datetime.time(hour = 5, minute = 30, second = 2)
+my_time
+
+# Can do math on datetime variables to get days until / since
+today = datetime.date.today()
+new_year = datetime.date(2025, 1, 1)
+countdown = new_year - today
+print(countdown.days, "days until the new year")
+
+# Can use "timedelta" function to get time until / since
+t1 = datetime.timedelta(weeks = 8, days = 4, hours = 3, seconds = 5)
+t2 = datetime.timedelta(days = 10, hours = 1, minutes = 10)
+t3 = t1 - t2
+t3
--- _exercises/16_exercises.py
+++ _exercises/16_exercises.py
@@ -1,19 +1,33 @@
 # Exercises - Date/Times
 
-# 1. Get the current day, month, year, hour, minute and timestamp from datetime module
+## Import module
+import datetime
 
+# 1. Get the current day, month, year, hour, minute and timestamp from datetime module
+now = datetime.datetime.now()
+now.day
+now.month
+now.year
+now.hour
+now.minute
+now.timestamp()
 
 # 2. Format the current date using this format: "%m/%d/%Y, %H:%M:%S")
-
+now_reformat = now.strftime("%m/%d/%Y, %H:%M:%S")
+now_reformat
 
 # 3. Today is 5 December, 2019. Change this time string to time.
-
+date_var = datetime.datetime.strptime("5 December, 2019", "%d %B, %Y")
+date_var
 
 # 4. Calculate the time difference between now and new year.
-
+new_year_diff =  datetime.datetime(year = 2025, month = 1, day = 1) - now
+new_year_diff.days
 
 # 5. Calculate the time difference between 1 January 1970 and now.
-
+jan_seventy = datetime.datetime(1970, 1, 1)
+time_since = now - jan_seventy
+time_since
 
 # 6. Think, what can you use the datetime module for? (E.g., time series analysis, get a timestamp of any activities in an application, adding posts on a blog, etc.)
 
