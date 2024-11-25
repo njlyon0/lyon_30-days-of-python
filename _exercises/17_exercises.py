9a3db700b5c1fd84820b296a796a65a5bb9a9b94
njlyon0 <nicholasjlyon@gmail.com>
2024-11-25 12:00:36 -0500

    feat (17): finished exercises

--- _exercises/17_exercises.py
+++ _exercises/17_exercises.py
@@ -1,2 +1,14 @@
-# Exercises - Exception Handling
+# Exercises - Exception Handling & Packing/Unpacking
 
+# 1. Unpack the first five countries and store them in a variable "nordic_countries", store Estonia and Russia in "es", and "ru" respectively.
+
+## Make list
+names = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland', 'Estonia', 'Russia']
+
+## Solve problem
+*nordic_countries, es, ru = names
+
+## check that worked
+nordic_countries
+es
+ru
