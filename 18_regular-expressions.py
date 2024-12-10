721719943c35b7fef6b6a9631a3f63b3a30f4cd2
njlyon0 <nicholasjlyon@gmail.com>
2024-12-10 14:31:52 -0500

    feat (18): partial notes on regex

--- 18_regular-expressions.py
+++ 18_regular-expressions.py
@@ -1,12 +1,28 @@
 """
 Regular Expressions
 
-
+Regular expressions are special text strings that can be used to find/interact with patterns in data
 """
 
+# Import relevant module
+import re
+
+# Make simple text demo
+ex_text = "I love to teach python and R"
+
+# "match" function searches the beginning of a string and returns either the matched characters or `None`
+re.match(pattern = "I love", string = ex_text)
+re.match(pattern = "I hate", string = ex_text)
+
+# "search"
 
 
+# "findall"
 
+# "split"
+
+
+# "sub"
 
 
 
