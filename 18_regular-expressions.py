c22c4552696e3016e45ecdedb9c0bec7779b7615
njlyon0 <nicholasjlyon@gmail.com>
2024-12-11 11:59:20 -0500

    feat (18): notes / exercises on regex

--- 18_regular-expressions.py
+++ 18_regular-expressions.py
@@ -8,21 +8,97 @@ Regular expressions are special text strings that can be used to find/interact w
 import re
 
 # Make simple text demo
-ex_text = "I love to teach python and R"
+ex_text = """I love teaching python and R.
+I enjoy working on the 30 day challenge"""
 
-# "match" function searches the beginning of a string and returns either the matched characters or `None`
-re.match(pattern = "I love", string = ex_text)
-re.match(pattern = "I hate", string = ex_text)
+# "match" function searches the *beginning* of a string and returns either the matched characters or `None`
+print(re.match(pattern = "I love", string = ex_text))
+print(re.match(pattern = "I enjoy", string = ex_text))
 
-# "search"
+# "search" returns a match if any is found at any point in even a multi-line string
+print(re.search(pattern = "I love", string = ex_text))
+print(re.search(pattern = "I enjoy", string = ex_text))
 
+# "findall" returns a list containing all matches
+print(re.findall(pattern = "I love", string = ex_text))
+print(re.findall(pattern = "I enjoy", string = ex_text))
 
-# "findall"
+# "split" accepts a string and splits it at the match points (returns as a list)
+## Note that it *removes* the matched part
+print(re.split(pattern = "I enjoy", string = ex_text))
+print(re.split(pattern = "\n", string = ex_text))
 
-# "split"
+# "sub" replaces one/many matches within a string
+print(re.sub(pattern = "I love", repl = "I LOVE", string = ex_text))
 
+# Can retrieve the coordinates of a match with the "span" method
+ex_match = re.search(pattern = "I enjoy", string = ex_text)
+ex_span = ex_match.span()
+ex_start, ex_end = ex_span
+ex_text[ex_start:ex_end]
 
-# "sub"
+# Multiple matches can be searched for at the same time
+re.findall(pattern = "love|enjoy", string = ex_text)
 
+# These matching functions *are case sensistive*!
+ex_text2 = "Python is a fun language, but 'python' is not always capitalized"
+re.findall(pattern = "Python", string = ex_text2)
 
+# Can handle differences in casing like so
+## If not naming parameters, can use the "I" attribute of the "re" module
+re.findall("Python", ex_text2, re.I)
+## Can also fully write out both options
+re.findall(pattern = "Python|python", string = ex_text2)
+## Can also put square brackets around the character(s) that differ
+re.findall(pattern = "[Pp]ython", string = ex_text2)
 
+""" Regex Patterns """
+
+# Can declare a RegEx variable with "r" followed by a string
+ex_text3 = "Apples and bananas are fruits. An old cliche says 'an apple a day keeps the doctor away' has been replaced by 'a banana a day keeps the doctor far far away'."
+
+regex_apple = r"[Aa]pple"
+
+re.findall(pattern = regex_apple, string = ex_text3)
+re.split(pattern = regex_apple, string = ex_text3)
+
+# There are many special facets of regex syntax we can leverage:
+## Can look for all letters between two specified letters
+r"[a-g]" 
+## Or the same for numbers
+r"[0-3]"
+## Or even multiple separate ranges of characters
+r"[A-F5-7g-j]"
+## Can use certain special characters if escaped with a backslash
+r"\d" ### match all digits (0-9)
+r"\D" ### match all *non-* digits
+## Find any character expect a newline
+r"[a]." ### finds groups of two characters where the first is "a"
+## Can find substrings
+r"^start" ### substring at start of a line
+r"end$" ### substring that ends a line
+## Can find replicates
+r"[x]+" ### one or more times
+r"[y]*" ### zero or more times
+r"[z]?" ### zero or one time
+### Can specify how any characters specifically
+r"[x]{3}" ### exactly three "x"s
+r"[y]{5,}" ### at least 5 "y"s
+r"[z]{1,6}" ### 1 to 6 "z"s
+
+# Let's practice finding all digits in the following string
+ex_text4 = "I am working on this lesson on December 11, 2024 and began this challenge more than 30 days ago"
+
+## Find all digits doesn't return exactly what we want
+re.findall(pattern = r"\d", string = ex_text4)
+
+## Can add a plus to keep groups of numbers together
+re.findall(pattern = r"\d+", string = ex_text4)
+
+# More practice on matches some number of times
+ex_text5 = '''I am not sure if there is a convention how to write the word e-mail.
+Some people write it as email others may write it as Email or E-mail.
+I am confident that it is not "e---mail" though.'''
+
+## "-?" means that only instances with one or zero hyphens are found (and not the "e---mail" at the end)
+re.findall(pattern = r"[Ee]-?mail", string = ex_text5)
--- _exercises/18_exercises.py
+++ _exercises/18_exercises.py
@@ -1,25 +1,82 @@
 # Exercises - Regular Expressions
 
+# Import needed module
+import re
+
 ## Exercises: Level 1
 ###  1. What is the most frequent word in the following paragraph?
 paragraph = 'I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.'
 
+# Find words in paragraph
+matches = re.findall(pattern = r"[A-Za-z]+", string = paragraph)
 
+# List for storing loop outputs
+done = list()
 
+# Set some useful starting positions
+max_word = ""
+max_ct = 0
+
+# Loop across words numbers
+for word_num in range(len(matches)):
+
+    # Find word at that index position
+    word = matches[word_num]
+
+    # Skip if already counted
+    if word in done: continue
+
+    # Otherwise...
+    else: 
+
+        # Count instances of that word
+        word_ct = matches.count(word)
+
+        # If this word occurs more than prior words:
+        if word_ct > max_ct:
+
+            # Update the max word & max count!
+            max_word = word
+            max_ct = word_ct
+
+        # Add to 'done' list
+        done.append(word)
+
+# Identify answer
+print("1-1: ", "The most used word is '", max_word, "' and it was used", max_ct, "times.")
 
 ### 2. The position of some particles on the horizontal x-axis are -12, -4, -3 and -1 in the negative direction, 0 at origin, 4 and 8 in the positive direction. Extract these numbers from this whole text and find the distance between the two furthest particles.
 points = ['-12', '-4', '-3', '-1', '0', '4', '8']
 
+# List for storing outputs
+points_vals = list()
 
+# Loop across points
+for pt in range(len(points)):
+    points_vals.append(int(points[pt]))
+
+# Identify distance between furthest points
+max_pt = max(points_vals)
+min_pt = min(points_vals)
+
+# Calculate distance
+pt_dist = max_pt - min_pt
+
+# Give answer
+print("1-2: ", "The distance between the max/min point (", max_pt, " and ", min_pt, ") is ", pt_dist)
 
 ## Exercises: Level 2
 ### 1. Write a pattern which identifies if a string is a valid python variable
+
 # Define function
 def is_valid_variable(name):
-    return "Maybe!"
 
+    # Identify regex patterns that make invalid variable names
+    bad = len(re.findall(pattern = "\d.+|-| |\?", string = name))
 
-
+    # Pass the appropriate boolean back to the user
+    if bad > 0: return False
+    else: return True
 
 # Invoke it
 is_valid_variable('first_name') # True
@@ -31,6 +88,40 @@ is_valid_variable('firstname') # True
 ### 1. Clean the following text. After cleaning, count three most frequent words in the string.
 sentence = '''%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?'''
 
-clean_text = sentence
+# Clean sentence
+clean_text = re.sub(pattern = "%|@|#|&|;|\$", repl = "", string = sentence)
+clean_text
 
-print(clean_text)
+# Find words in sentence
+matches = re.findall(pattern = r"[A-Za-z]+", string = clean_text)
+matches
+
+# Lists for storing loop outputs
+done = list()
+match_cts = list()
+
+# Loop across words numbers
+for word_num in range(len(matches)):
+
+    # Find word at that index position
+    word = matches[word_num]
+
+    # Skip if already counted
+    if word in done:
+        continue
+
+    # Otherwise...
+    else: 
+
+        # Count instances of that word
+        word_ct = matches.count(word)
+
+        # Add to 'done' lists
+        match_cts.append([word, word_ct])
+        done.append(word)
+
+# See output list
+match_cts
+
+# Identify answer
+print("3-1:", "The ost common word is 'I' and occurs 3 times")
