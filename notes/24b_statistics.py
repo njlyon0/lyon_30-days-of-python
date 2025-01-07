2ed39052059f9bd53fb8c3ae8400ef663dc78a6a
njlyon0 <nicholasjlyon@gmail.com>
2025-01-07 15:21:59 -0500

    style (24): renamed 'statistics' notes to separate from fundamental numpy review

---
+++ notes/24b_statistics.py
@@ -0,0 +1,14 @@
+"""
+Statistics
+
+"Statistics" is a very broad term that encapsulates a lot of territory but generally contains many tools built to summarize or compare data. These tools are prerequisite to both data science generally and machine learning approaches more specifically.
+
+Python includes a "statistics" module for doing simple mathematical statistics on numeric data. It can be valuable but is not meant to compete with either more nuanced Python packages (e.g., NumPy, SciPy, MatPlotLib, etc.) or standalone non-Python statistical languages (e.g., R, SAS, Matlab, etc.).
+
+These notes are mostly focused on the "numpy" module but delve into the "statistics" module as needed.
+"""
+
+# Import needed module(s)
+import statistics
+import numpy as np
+
