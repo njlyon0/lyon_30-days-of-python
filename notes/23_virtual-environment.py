3d26924a5546c09ec717e189f9e40e108aea470f
njlyon0 <nicholasjlyon@gmail.com>
2025-01-07 13:07:26 -0500

    feat (23): notes on virtual environments

--- notes/23_virtual-environment.py
+++ notes/23_virtual-environment.py
@@ -1,9 +1,20 @@
 """
 Virtual Environments
 
+Virtual environents help create siloed spaces for discrete projects. This makes identifying dependencies unique to a particular project (rather than all packages ever installed on your computer) much simpler. 
 """
 
+# You can use the "virtualenv" package for this
+## Installed via command line as follows:
+## `python3 -m pip install virtualenv`
 
+# Virtual environments can then be instantiated with the following comand line syntax
+## `virtualenv VIRTUALENVIRONMENTNAME`
 
-# Skipping (for now)
-## Full day's 'challenge' here: https://github.com/Asabeneh/30-Days-Of-Python/blob/master/23_Day_Virtual_environment/23_virtual_environment.md
+# Once created, they can be activated like so:
+## `source VIRTUALENVIRONMENTNAME/bin/activate`
+
+# From here on out, your command line lines should start with "(venv)" instead of "(base)" (or nothing)
+
+# When finished working, it is good practice to then deactivate the virtual environment
+## `deactivate`
